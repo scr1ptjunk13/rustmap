16f23941894047496b1d938df34d06c2a5be6905
scr1ptjunk13 <itsameshreyansh@protonmail.com>
2025-07-24 13:26:23 +0530

    changed rustmap.sh

--- rustmap.sh
+++ rustmap.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-echo "=== Building RustMap ==="
-cargo build --release
 
 echo -e "\n=== Basic Local Scan ==="
 cargo run -- -a 127.0.0.1
 
-cargo run -- -a 127.0.0.1 -r 1-500 -b 1000
 
 echo -e "\n=== Greppable Output ==="
-cargo run -- -a 127.0.0.1 -r 1-100 -g
+cargo run -- -a 127.0.0.1 -g
 
 
 
-#DNS output
-sudo cargo run -- --udp -a 192.168.31.210 -p 53,67,68,69,123,161 -b 500 -t 5000
-
-
-echo -e "\n=== Scan Public DNS ==="
+echo -e "\n=== Scan Public DNS ===" //range capability
 cargo run -- -a 8.8.8.8 -r 50-100
 
 #Python UDP responder
@@ -37,10 +30,9 @@ scanme.nmap.org
 cargo run -- -a scanme.nmap.org -r 1-100
 
 
+//time :
 
 time cargo run -- -a 127.0.0.1
-
-time cargo run -- -a 127.0.0.1 -p --ulimit 10000 --timeout 150 --greppable
 
 
 
@@ -51,9 +43,13 @@ time cargo run -- -a 127.0.0.1 -p --ulimit 10000 --timeout 150 --greppable
 ./cargorun -- -a 192.168.1.1 -r 53 --udp #53/udp - DOMAIN
 ./cargorun -- -a 192.168.1.1 -r 69 --udp #69/udp - TFTP
 
+
+
+
+
 #for time checking and comparison
-time nmap -p- scanme.nmap.org
-time cargo run -- -a scanme.nmap.org -r 1-65535 --timeout 1000 --batch-size 5000 -- -Pn
+#time nmap -p- scanme.nmap.org
+#time cargo run -- -a scanme.nmap.org -r 1-65535 --timeout 1000 --batch-size 5000 -- -Pn
 
 #If a port is closed, the host may respond with ICMP Port Unreachable — which scanner looks for.
 #Firewalls often drop UDP traffic silently, making results ambiguous (filtered vs. open|filtered).
